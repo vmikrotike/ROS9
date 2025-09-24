4bb6c79a3afb122cf6324396f6093da4bc5e3be7
vmikrotike <vmikrotike@gmail.com>
2025-09-24 11:34:27 +0300

    Add files via upload

---
+++ chr.sh
@@ -0,0 +1,15 @@
+curl -# -L  https://github.com/elseif/MikroTikPatch/releases/download/7.19.1/chr-7.19.1-legacy-bios.img.zip -o /tmp/chr.img.zip  && cd /tmp && \
+gunzip -c chr.img.zip > chr.img  && \
+STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
+echo STORAGE is $STORAGE && \
+ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
+echo ETH is $ETH && \
+ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
+echo ADDRESS is $ADDRESS && \
+GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
+echo GATEWAY is $GATEWAY && \
+sleep 5 && \
+dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
+echo "Ok, reboot" && \
+echo 1 > /proc/sys/kernel/sysrq && \
+echo b > /proc/sysrq-trigger
